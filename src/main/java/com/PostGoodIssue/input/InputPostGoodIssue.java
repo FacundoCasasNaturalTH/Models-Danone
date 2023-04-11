a609ec56e6b6b1206fa592ce8096587af23ff1ca
Facundo Casas <facundo.casas@naturalth.it>
2023-04-11 16:22:28 -0300

    Change JsonProperties

--- src/main/java/com/PostGoodIssue/input/InputPostGoodIssue.java
+++ src/main/java/com/PostGoodIssue/input/InputPostGoodIssue.java
@@ -3,113 +3,92 @@ package com.PostGoodIssue.input;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class InputPostGoodIssue {
+    @JsonProperty("LINUM")
     private String linum;
+    @JsonProperty("DLIEF")
     private String dlief;
+    @JsonProperty("WERKS")
     private String werks;
+    @JsonProperty("NUM")
     private String num;
+    @JsonProperty("MEINS")
     private String meins;
+    @JsonProperty("LGMNG")
     private String lgmng;
+    @JsonProperty("VRKME")
     private String vrkme;
+    @JsonProperty("GLIEF1")
     private String glief1;
+    @JsonProperty("PRODLOT")
     private String prodlot;
+    @JsonProperty("EXPIRY")
     private String expiry;
+    @JsonProperty("SSCC")
     private String sscc;
 
-    @JsonProperty("LINUM")
     public String getLinum() {
         return linum;
     }
-    @JsonProperty("LINUM")
     public void setLinum(String linum) {
         this.linum = linum;
     }
-
-    @JsonProperty("DLIEF")
     public String getDlief() {
         return dlief;
     }
-    @JsonProperty("DLIEF")
     public void setDlief(String dlief) {
         this.dlief = dlief;
-    }
-
-    @JsonProperty("WERKS")
+    } 
     public String getWerks() {
         return werks;
     }
-    @JsonProperty("WERKS")
     public void setWerks(String werks) {
         this.werks = werks;
     }
-
-    @JsonProperty("NUM")
     public String getNum() {
         return num;
     }
-    @JsonProperty("NUM")
     public void setNum(String num) {
         this.num = num;
     }
-
-    @JsonProperty("MEINS")
     public String getMeins() {
         return meins;
     }
-    @JsonProperty("MEINS")
     public void setMeins(String meins) {
         this.meins = meins;
-    }
-
-    @JsonProperty("LGMNG")
+    } 
     public String getLgmng() {
         return lgmng;
     }
-    @JsonProperty("LGMNG")
     public void setLgmng(String lgmng) {
         this.lgmng = lgmng;
     }
-
-    @JsonProperty("VRKME")
     public String getVrkme() {
         return vrkme;
     }
-    @JsonProperty("VRKME")
     public void setVrkme(String vrkme) {
         this.vrkme = vrkme;
     }
-
-    @JsonProperty("GLIEF1")
     public String getGlief1() {
         return glief1;
     }
-    @JsonProperty("GLIEF1")
     public void setGlief1(String glief1) {
         this.glief1 = glief1;
     }
-
-    @JsonProperty("PRODLOT")
     public String getProdlot() {
         return prodlot;
     }
-    @JsonProperty("PRODLOT")
     public void setProdlot(String prodlot) {
         this.prodlot = prodlot;
     }
-
-    @JsonProperty("EXPIRY")
     public String getExpiry() {
         return expiry;
     }
-    @JsonProperty("EXPIRY")
     public void setExpiry(String expiry) {
         this.expiry = expiry;
     }
-
-    @JsonProperty("SSCC")
     public String getSscc() {
         return sscc;
     }
-    @JsonProperty("SSCC")
     public void setSscc(String sscc) {
         this.sscc = sscc;
     }
